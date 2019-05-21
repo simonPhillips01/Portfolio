b8bb85f59b590dfba55ff88ce2f706c5ffd3733b
Simon Phillips <simon.phillips01@icloud.com>
2019-05-21 09:25:39 +0100

    Added navigation styling

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -14,7 +14,7 @@ import {
 
     return (
       <Link href={route}>
-        <a className="nav-link">{ title }</a>
+        <a className="nav-link port-navbar-link">{ title }</a>
       </Link>
     )
   } 
@@ -36,24 +36,24 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Simon Phillips</NavbarBrand>
+        <Navbar className="port-navbar port-default absolute" color="transparent" dark expand="md">
+          <NavbarBrand className="port-navbar-brand" href="/">Simon Phillips</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
+              <NavItem className="port-navbar-item">
                 <BsNavLink route="/" title="Home" />
               </NavItem>
-              <NavItem>
+              <NavItem className="port-navbar-item">
                 <BsNavLink route="/about" title="About" />
               </NavItem>
-              <NavItem>
+              <NavItem className="port-navbar-item">
                 <BsNavLink route="/portfolio" title="Portfolio" />
               </NavItem>
-              <NavItem>
+              <NavItem className="port-navbar-item">
                 <BsNavLink route="/blog" title="Blog" />
               </NavItem>
-              <NavItem>
+              <NavItem className="port-navbar-item">
                 <BsNavLink route="/cv" title="CV" />
               </NavItem>
             </Nav>
