5816666bc1d432e7321d7134e79545e08bf8750a
Simon Phillips <simon.phillips01@icloud.com>
2019-05-28 16:30:20 +0100

    Created server folder with secret endpoint

--- pages/secret.js
+++ pages/secret.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
+import axios from 'axios';
 
 class Secret extends React.Component {
 
@@ -11,13 +12,48 @@ class Secret extends React.Component {
     return { superSecretValue };
   }
 
+  constructor(props) {
+    super();
+
+    this.state = {
+      secretData: []
+    }
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('/api/v1/secret');
+    const secretData = res.data;
+
+    this.setState({
+      secretData
+    });
+  }
+
+  displaySecretData() {
+    const { secretData } = this.state;
+
+    if(secretData && secretData.length > 0) {
+      return secretData.map((data, index) => {
+        return (
+          <div key={index}>
+            <p>{ data.title }</p>
+            <p>{ data.description }</p>
+          </div>
+        )
+      })
+    }
+    return null;
+  }
+
   render() {
     const { superSecretValue } = this.props;
+
     return (
       <BaseLayout {...this.props.auth}>
           <BasePage>
             <h1>I am the secret page</h1>
             <h2> {superSecretValue} </h2>
+            { this.displaySecretData() }
           </BasePage>
         </BaseLayout>
     )
