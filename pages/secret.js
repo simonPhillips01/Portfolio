9f9e22d4212a4e750dff6fde4e7c3b2549ae2f1e
Simon Phillips <simon.phillips01@icloud.com>
2019-05-29 22:05:16 +0100

    Axios Auth Headers

--- pages/secret.js
+++ pages/secret.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
-import axios from 'axios';
+import { getSecretData } from'../actions/index';
 
 class Secret extends React.Component {
 
@@ -20,9 +20,12 @@ class Secret extends React.Component {
     }
   }
 
+  // state = {
+  //   secretData: []
+  // }
+
   async componentDidMount() {
-    const res = await axios.get('/api/v1/secret');
-    const secretData = res.data;
+    const secretData = await getSecretData();
 
     this.setState({
       secretData
