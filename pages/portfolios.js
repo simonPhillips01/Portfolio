8b7ff54b0b109a66e4e5cfb85fce14807b258258
Simon Phillips <simon.phillips01@icloud.com>
2019-06-18 13:15:42 +0100

    Stop propagation on click

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -22,7 +22,13 @@ class Portfolios extends React.Component {
     return {portfolios};
   }
 
-  displayDeleteWarning(portfolioId) {
+  navigateToEdit(portfolioId, e) {
+    e.stopPropagation();
+    Router.pushRoute(`/portfolios/${portfolioId}/edit`)
+  }
+
+  displayDeleteWarning(portfolioId, e) {
+    e.stopPropagation();
     const isConfirm = confirm('Are you sure you want to delete this portfolio???');
 
     if(isConfirm) {
@@ -48,8 +54,8 @@ class Portfolios extends React.Component {
               <PortfolioCard portfolio={portfolio}>
               { isAuthenticated && isSiteOwner &&
                     <React.Fragment>
-                    <Button onClick={() => Router.pushRoute(`/portfolios/${portfolio._id}/edit`)} color="warning">Edit</Button>{' '}
-                    <Button onClick={() => this.displayDeleteWarning(portfolio._id)} color="danger">Delete</Button>
+                    <Button onClick={(e) => this.navigateToEdit(portfolio._id, e)} color="warning">Edit</Button>{' '}
+                    <Button onClick={(e) => this.displayDeleteWarning(portfolio._id, e)} color="danger">Delete</Button>
                     </React.Fragment>
               }
               </PortfolioCard>
