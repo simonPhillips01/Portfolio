40078cb480d6ac18e0894ba638ff70a8215a6f67
Simon Phillips <simon.phillips01@icloud.com>
2019-06-19 02:40:47 +0100

    Blog styling

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Header from '../shared/Header';
+import Head from 'next/head';
 
 const BaseLayout = (props) => {
   const { className, children, isAuthenticated } = props;
   const headerType = props.headerType || 'default';
   return (
-    <div className="layout-container">
-      {/* {headerType === 'index' && <Header className="port-nav-index" isAuthenticated={isAuthenticated}/>}
-      {headerType === 'default' && <Header className="port-nav-default" isAuthenticated={isAuthenticated}/>} */}
-      <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated}/>
-      <main className={`cover ${className}`}>
-        <div className="wrapper">
-          {children}
+    <React.Fragment>
+      <Head>
+        <title>Simon Phillips</title>
+        <script src="https://kit.fontawesome.com/76b070cc82.js"></script>
+      </Head>
+        <div className="layout-container">
+          {/* {headerType === 'index' && <Header className="port-nav-index" isAuthenticated={isAuthenticated}/>}
+          {headerType === 'default' && <Header className="port-nav-default" isAuthenticated={isAuthenticated}/>} */}
+          <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated}/>
+          <main className={`cover ${className}`}>
+            <div className="wrapper">
+              {children}
+            </div>
+          </main>
         </div>
-      </main>
-    </div>
+    </React.Fragment>
   )
 }
 
