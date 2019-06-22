3361cdc8f7d26df0715979786956aa1d75e2a514
Simon Phillips <simon.phillips01@icloud.com>
2019-06-22 11:06:51 +0100

    Added cv via iframe to cv page

--- pages/cv.js
+++ pages/cv.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
+import { Row, Col } from 'reactstrap';
 
 class CV extends React.Component {
   render() {
     return (
       <div>
         <BaseLayout {...this.props.auth} headerType="cv">
-          <BasePage>
-            <h1>I am the cv page</h1>
+          <BasePage title="Read my CV" className="cv-page">
+            <Row>
+              <Col md={{size: 8, offset: 2}}>
+                <div className="cv-title">
+                  <a download="Simon Phillips - Javascript developer.pdf" className="btn btn-success" href="/static/Simon Phillips - Javascript Developer.pdf">
+                    Download
+                  </a>
+                </div>
+                <iframe style={{width: '100%', height: '800px'}} src="/static/Simon Phillips - Javascript Developer.pdf"></iframe>
+              </Col>
+            </Row>
           </BasePage>
         </BaseLayout>
       </div>
