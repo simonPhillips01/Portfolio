def17b56d7c428099934d455fbbb72c1dcb4768c
Simon Phillips <simon.phillips01@icloud.com>
2019-06-22 12:50:13 +0100

    Added event handler

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -36,6 +36,17 @@ export default class SlateEditor extends React.Component {
       this.setState({ value })
     }
 
+    onKeyDown = (event, editor, next) => {
+        // Return with no changes if the keypress is not '&'
+        if (event.key !== '&') return next()
+
+        // Prevent the ampersand character from being inserted.
+        event.preventDefault()
+        
+        // Change the value by inserting 'and' at the cursor's position.
+        editor.insertText('and')
+    }
+
     // Render the editor.
     render() {
         const { isLoaded } = this.state;
@@ -43,7 +54,9 @@ export default class SlateEditor extends React.Component {
       return (
           <React.Fragment>
             { isLoaded &&
-            <Editor value={this.state.value} onChange={this.onChange} />
+            <Editor value={this.state.value}
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyDown} />
             }
           </React.Fragment>
       )
