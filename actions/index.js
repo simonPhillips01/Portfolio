4ba74ee0d39bef97a8913cfa3b8931b5d0268c0e
Simon Phillips <simon.phillips01@icloud.com>
2019-06-27 17:21:04 +0100

    Update blog functionality

--- actions/index.js
+++ actions/index.js
@@ -67,6 +67,12 @@ export const createBlog = (blogData, lockId) => {
         .catch(err => rejectPromise(err))
 }
 
+export const updateBlog = (blogData, blogId) => {
+    return axiosInstance.patch(`/blogs/${blogId}`, blogData, setAuthHeader())
+        .then(response => response.data)
+        .catch(err => rejectPromise(err))
+}
+
 export const getBlogById = (blogId) => {
     return axiosInstance.get(`/blogs/${blogId}`).then(response => response.data);
 }
--- pages/blogEditorUpdate.js
+++ pages/blogEditorUpdate.js
@@ -5,7 +5,7 @@ import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
 import SlateEditor from '../components/slate-editor/Editor';
 
-import { getBlogById } from '../actions';
+import { getBlogById, updateBlog } from '../actions';
 
 class BlogEditorUpdate extends React.Component {
     
@@ -28,6 +28,27 @@ class BlogEditorUpdate extends React.Component {
         this.state = {
             isSaving: false
         }
+
+        this.updateBlog = this.updateBlog.bind(this);
+    }
+
+    updateBlog(story, heading) {
+        const {blog} = this.props;
+
+        const updatedBlog = {};
+        updatedBlog.title = heading.title;
+        updatedBlog.subTitle = heading.subtitle;
+        updatedBlog.story = story;
+
+        this.setState({isSaving: true});
+
+        updateBlog(updatedBlog, blog._id).then(updatedBlog => {
+            this.setState({isSaving: false});
+        }).catch(err => {
+            this.setState({isSaving: false});
+            const message = err.message || 'Server Error!';
+            console.error(message);
+        })
     }
     
     render() {
@@ -37,7 +58,7 @@ class BlogEditorUpdate extends React.Component {
         return (
             <BaseLayout {...this.props.auth}>
                 <BasePage containerClass="editor-wrapper" className="blog-editor-page">
-                <SlateEditor initialValue={blog.story} isLoading={isSaving} save={() => { console.log('Update should be here')}}/>
+                <SlateEditor initialValue={blog.story} isLoading={isSaving} save={this.updateBlog}/>
                 </BasePage>
             </BaseLayout>
         )
