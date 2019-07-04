bed922d3ff2e8fe40d2627f1f80a9fc43305759c
Simon Phillips <simon.phillips01@icloud.com>
2019-07-04 13:16:18 +0100

    Added links

--- pages/blog.js
+++ pages/blog.js
@@ -79,15 +79,7 @@ class Blog extends React.Component {
                   <div className="col-lg-8 col-md-10 mx-auto">
                     <ul className="list-inline text-center">
                       <li className="list-inline-item">
-                        <a href="#">
-                          <span className="fa-stack fa-lg">
-                            <i className="fas fa-circle fa-stack-2x"></i>
-                            <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                          </span>
-                        </a>
-                      </li>
-                      <li className="list-inline-item">
-                        <a href="#">
+                        <a target="_blank" href="https://www.facebook.com/simon.phillips.338">
                           <span className="fa-stack fa-lg">
                             <i className="fas fa-circle fa-stack-2x"></i>
                             <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -95,7 +87,7 @@ class Blog extends React.Component {
                         </a>
                       </li>
                       <li className="list-inline-item">
-                        <a href="#">
+                        <a target="_blank" href="https://github.com/simonPhillips01">
                           <span className="fa-stack fa-lg">
                             <i className="fas fa-circle fa-stack-2x"></i>
                             <i className="fab fa-github fa-stack-1x fa-inverse"></i>
