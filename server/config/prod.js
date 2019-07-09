0248e4f57af71f39376c91235829d0e3c7150eb0
Simon Phillips <simon.phillips01@icloud.com>
2019-07-09 12:45:22 +0100

    Changed production configuration

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,7 +1,3 @@
-// module.exports = {
-//     DB_URI: process.env.DB_URI
-// }
-
 module.exports = {
-    DB_URI: 'mongodb+srv://admin:Testtest1@portfolio-ookex.mongodb.net/test?retryWrites=true&w=majority'
+    DB_URI: process.env.DB_URI
 }
