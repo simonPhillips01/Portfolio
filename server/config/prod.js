14b7458ea756969ff417c9bca91429a70e090d60
Simon Phillips <simon.phillips01@icloud.com>
2019-07-16 08:17:02 +0100

    Removed / from namespace

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_URI: process.env.DB_URI,
-    NAMESPACE: 'https://simon-phillips-portfolio.herokuapp.com/'
+    NAMESPACE: 'https://simon-phillips-portfolio.herokuapp.com'
 }
